3c16bf07378cd147f9ae1dffe5fb3f5ef4c60300
aandremeleti-hub <aandremeleti@email.com>
2025-12-27 18:03:54 -0300

    style: main stylization

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
 import { ModalMovie } from "../modals/modalMovie/modalMovie"
+import './main.css' 
 
 export const Main = () => {
 
